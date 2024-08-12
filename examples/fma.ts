6c9f73c423116ade9a771c97b4b535f5e21372be
Gregor <gregor.mitscha-baude@gmx.at>
2024-08-12 16:18:24 +0200

    more fma

--- examples/fma.ts
+++ examples/fma.ts
@@ -10,15 +10,26 @@ const fma = func({ in: [f64, f64, f64], out: [f64] }, ([x, y, z]) => {
   f64x2.extract_lane(0);
 });
 
-let module = Module({ exports: { fma } });
+const fnma = func({ in: [f64, f64, f64], out: [f64] }, ([x, y, z]) => {
+  f64x2.splat(x);
+  f64x2.splat(y);
+  f64x2.splat(z);
+  f64x2.relaxed_nmadd();
+  f64x2.extract_lane(0);
+});
+
+let module = Module({ exports: { fma, fnma } });
 let { instance } = await module.instantiate();
 
 console.dir(module.module, { depth: Infinity });
 
-let [x, y, z] = [0.5, 3, 1.5];
+let [x, y, z] = [0.5, 3, 1.25];
 
-let result = instance.exports.fma(x, y, z);
+let r1 = instance.exports.fma(x, y, z);
+let r2 = instance.exports.fnma(x, y, z);
 
-assert.equal(result, x * y + z);
-console.log({ result });
-// { result: 0.5 * 3 + 1.5 = 3 }
+assert.equal(r1, x * y + z);
+assert.equal(r2, -(x * y) + z);
+
+console.log({ r1, r2 });
+// { r1: 2.75, r2: -0.25 }
